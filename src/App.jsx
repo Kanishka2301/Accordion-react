c7e7dc66d0a2717479d28103e7e726af83e77d78
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-03-15 21:37:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Accordian from "./components/accordian";
+
+function App() {
+  return (
+    <div classname="App">
+      <Accordian />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/accordian/data.js
@@ -0,0 +1,27 @@
+const data = [
+  {
+    id: "1",
+    question: "What are accordion components?",
+    answer:
+      "Accordion components are user interface elements,it lets user show and hide sections of related content on a page",
+  },
+  {
+    id: "2",
+    question: "What are they used for?",
+    answer:
+      "They are commonly employed in various conexts,including FAQs and used to generate vertically stacked content in a page",
+  },
+  {
+    id: "3",
+    question: "How is accordion as a musical instrument?",
+    answer:
+      "Accordions are a family of box-shaped musical instruments of the bellows-driven free reed aerophone type",
+  },
+  {
+    id: "4",
+    question: "What is an accordion in react?",
+    answer:
+      "An accordion is a vertically collapsing component that allows users to view one section of content at a time",
+  },
+];
+export default data;
